e249ea808bf3c070634ab207334af12bb452d893
Unknown <mdalexisaguilera@gmail.com>
2017-07-25 20:20:00 -0300

    algo

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,29 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('stylesheets/screen.css'));
-
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+  .pipe(gulp.dest('./stylesheets'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-
-
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', () =>
-    gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('stylesheets/screen.css'))
+gulp.task('optimizar', () =>
+    gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('images'))
 );
+
+
+
+gulp.task('default', ['sass', 'watch']);
